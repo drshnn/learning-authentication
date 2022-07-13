d49aae25098f7faad6409883dae13a68e3b192c5
drshnn <darshansu156@gmail.com>
2022-07-13 21:34:39 +0530

    still some error at private routes, working on it

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,35 @@
-import logo from "./logo.svg";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
+//Routing
+import PrivateRoute from "./components/routing/PrivateRoute";
+import PrivateScreen from "./components/screens/PrivateScreen";
+import LoginScreen from "./components/screens/LoginScreen";
+import RegisterScreen from "./components/screens/RegisterScreen";
+import ForgotPasswordScreen from "./components/screens/ForgotPasswordScreen";
+import ResetPasswordScreen from "./components/screens/ResetPasswordScreen";
 function App() {
-  return <div className="App">something</div>;
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <PrivateRoute exact path="/" component={PrivateScreen} />
+          <Route exact path="/login" component={LoginScreen} />
+          <Route exact path="/register" component={RegisterScreen} />
+          <Route
+            exact
+            path="/forgotpassword"
+            component={ForgotPasswordScreen}
+          />
+          <Route
+            exact
+            path="/passwordreset/:resetToken"
+            component={ResetPasswordScreen}
+          />
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ client/src/components/screens/ResetPasswordScreen.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const ResetPasswordScreen = () => {
+  return <div>ResetPasswordScreen</div>;
+};
