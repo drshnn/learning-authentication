828832d88caa69b7e22c8f4d41cfbe032bd8b0b8
drshnn <darshansu156@gmail.com>
2022-07-18 18:22:29 +0530

    Login page and missing page

--- client/src/components/screens/MissingScreen.jsx
+++ client/src/components/screens/MissingScreen.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function MissingScreen() {
-  return <div>MissingScreen</div>;
+  return (
+    <div className="container">
+      <h1 className="text-4xl font-bold mb-2">Error 404</h1>
+      <p className="">The page you are trying to access does not exist.</p>
+    </div>
+  );
 }
 
 export default MissingScreen;
