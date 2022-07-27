7e8d754b6950a682decbc05e9f8729d47472e910
drshnn <darshansu156@gmail.com>
2022-07-27 14:22:26 +0530

    private screen completed

--- client/src/components/screens/PrivateScreen.jsx
+++ client/src/components/screens/PrivateScreen.jsx
@@ -23,11 +23,24 @@ function PrivateScreen() {
     }
   }, [user, isError, isSuccess, message, navigate, dispatch]);
   return (
-    <div className="flex flex-col gap-5">
-      <p>PrivateScreen</p>
-      <button onClick={logoutHandler} className="">
-        Logout
-      </button>
+    <div>
+      <div className="flex justify-between py-6 ">
+        <p className="font-bold text-3xl">AUTH</p>
+        <button
+          onClick={logoutHandler}
+          className="px-5 py-2 bg-blue-600 text-white rounded-md"
+        >
+          Logout
+        </button>
+      </div>
+      <div className="flex justify-center items-center pt-52">
+        <p className="text-5xl font-bold">
+          Welcome,{" "}
+          <span className=" text-5xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r via-purple-600 from-blue-500 to-pink-600">
+            {user && user.userData.username}
+          </span>
+        </p>
+      </div>
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -9,7 +9,6 @@ module.exports = {
       transparent: 'transparent',
       current: 'currentColor',
       'white': '#ffffff',
-      'purple': '#3f3cbb',
       'midnight': '#121063',
       'metal': '#565584',
       'tahiti': '#3ab7bf',
